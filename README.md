5a11eb90e747022d261045defa902d9e5e50bb57
Radek Poskocil <r.poskocil@quadient.com>
2021-06-16 12:41:43 +0200

    Loono BE init commit - Gradle / Kotlin / JVM

---
+++ README.md
@@ -0,0 +1,3 @@
+<p align="center">
+    <img src="https://www.loono.cz/files/logo-loono-colour-01.svg" alt="Logo" width="80"> Backend
+</p>
---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+group "cz.loono"
+version "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
