f6b6140cb54135a6fdc6ce42e409a7f5dba8a5d1
Radek Poskocil <r.poskocil@quadient.com>
2021-07-20 12:36:43 +0200

    Removed basic auth over healthcheck

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-security")
 
     implementation("io.springfox:springfox-boot-starter:3.0.0")
     implementation("io.springfox:springfox-swagger-ui:3.0.0")
