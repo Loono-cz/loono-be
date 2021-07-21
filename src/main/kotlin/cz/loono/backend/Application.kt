6e7f9fb5fc16465e30034dc8c78280c3e7c5d305
Radek Poskocil <r.poskocil@quadient.com>
2021-07-21 21:47:17 +0200

    Uptime server metric

--- src/main/kotlin/cz/loono/backend/Application.kt
+++ src/main/kotlin/cz/loono/backend/Application.kt
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 @SpringBootApplication
 @EntityScan(basePackages = ["cz.loono.backend.data.model"])
 @EnableJpaRepositories(basePackages = ["cz.loono.backend.data.repository"])
-@ComponentScan(basePackages = ["cz.loono.backend.api.actuator"])
+@ComponentScan(basePackages = ["cz.loono.backend.api.actuator", "org.springframework.boot.actuate.metrics"])
 class Application
 
 fun main(args: Array<String>) {
