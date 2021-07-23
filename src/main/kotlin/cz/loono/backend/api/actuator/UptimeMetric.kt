5b09b5014e028aba6ce2d9bf137eb0c5fbf89d21
Radek Poskocil <r.poskocil@quadient.com>
2021-07-23 08:53:33 +0200

    Fixed uptime metric

--- src/main/kotlin/cz/loono/backend/api/actuator/UptimeMetric.kt
+++ src/main/kotlin/cz/loono/backend/api/actuator/UptimeMetric.kt
@@ -1,18 +1,15 @@
 package cz.loono.backend.api.actuator
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.actuate.health.Health
 import org.springframework.boot.actuate.health.HealthIndicator
-import org.springframework.boot.actuate.metrics.MetricsEndpoint
 import org.springframework.stereotype.Component
+import java.lang.management.ManagementFactory
 
 @Component
-class UptimeMetric @Autowired constructor(
-    private val metricsEndpoint: MetricsEndpoint
-) : HealthIndicator {
+class UptimeMetric : HealthIndicator {
 
     override fun health(): Health {
-        val uptime = metricsEndpoint.metric("process.uptime", null).measurements
-        return Health.up().withDetail("Server uptime", uptime[0].value).build()
+        val mxBean = ManagementFactory.getRuntimeMXBean()
+        return Health.up().withDetail("Server uptime", mxBean.uptime).build()
     }
 }
