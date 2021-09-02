b3697b6b3e7e5e93aaa417526157fcdcee17aeb1
Ondra Karmazín <ondrej.karmazin@outlook.com>
2021-09-02 15:48:51 +0200

    Introduce OpenAPI generator (#8)
    Generates DTO models from the OpenAPI spec
    
    Run `gradlew downloadOpenApiSpec` to update the document.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("org.springframework.boot") version "2.5.3"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
+    id("org.openapi.generator") version "5.2.1"
+    id("de.undercouch.download") version "4.1.2"
     kotlin("jvm") version "1.5.20"
     kotlin("plugin.spring") version "1.5.20"
     jacoco
@@ -20,6 +22,7 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
@@ -56,5 +59,57 @@ tasks.withType<Test> {
 tasks.jacocoTestReport {
     reports {
         csv.required.set(true)
+    }
+}
+
+setUpOpenApiGenerator()
+
+fun setUpOpenApiGenerator() {
+    tasks.withType<org.jlleitschuh.gradle.ktlint.tasks.KtLintCheckTask> {
+        dependsOn("openApiGenerate")
+    }
+
+    tasks.compileKotlin.configure {
+        dependsOn("openApiGenerate")
+    }
+
+    val localSpecFile = projectDir.toPath().resolve("src/main/resources/doc/openapi.json")
+
+    tasks.register<de.undercouch.gradle.tasks.download.Download>("downloadOpenApiSpec") {
+        src("https://stoplight.io/api/v1/projects/okarmazin/loono/nodes/openapi.json")
+        dest(localSpecFile.toFile())
+    }
+
+    openApiGenerate {
+        val outputPath = project.buildDir.toPath().resolve("generated/openapi")
+        val sourceSetPath = outputPath.resolve("src/main/kotlin")
+
+        sourceSets.main {
+            java.srcDir(sourceSetPath)
+        }
+
+        ktlint {
+            filter {
+                exclude { element ->
+                    if (element.isDirectory) return@exclude false
+
+                    val file = sourceSetPath.resolve(element.path).toFile()
+                    file.exists()
+                }
+            }
+        }
+
+        outputDir.set(outputPath.toString())
+        modelPackage.set("cz.loono.backend.api.dto")
+        modelNameSuffix.set("Dto")
+        generatorName.set("kotlin-spring")
+        globalProperties.set(
+            mapOf(
+                "apis" to "false",
+                "models" to "",
+                "enumPropertyNaming" to "original",
+            )
+        )
+        inputSpec.set(localSpecFile.toString())
     }
 }
