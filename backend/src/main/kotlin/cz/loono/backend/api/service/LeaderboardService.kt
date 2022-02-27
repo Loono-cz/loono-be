149d55ecc058818601acf30c9f884686423a7174
Dan Zaru <d35h@users.noreply.github.com>
2022-02-27 08:43:28 +0100

    fix-complexity: Fix readability and complexity of leaderboard calculation (#62)
    * fix-complexity: Fix complexity of leaderboard calculation
    
    * fix-complexity: Refactor to more idiomatic way
    
    * fix-complexity: Refactor to more idiomatic way

--- backend/src/main/kotlin/cz/loono/backend/api/service/LeaderboardService.kt
+++ backend/src/main/kotlin/cz/loono/backend/api/service/LeaderboardService.kt
@@ -12,46 +12,26 @@ class LeaderboardService(
     private val accountRepository: AccountRepository
 ) {
 
-    private val batchSize = 500
-
-    fun getLeaderboard(uid: String): LeaderboardDto {
-
-        val top3 = findTop3Accounts(uid)
-
-        val iterations = accountRepository.count() / batchSize
-        var userOrder = 0L
-        val peers = mutableListOf<LeaderboardUserDto>()
-        iter@ for (i in 0..iterations) {
-            val accountsBatch = accountRepository.findAllByOrderByPointsDesc(PageRequest.of(i.toInt(), batchSize))
-            for (j in accountsBatch.indices) {
-                if (accountsBatch[j].uid == uid) {
-                    userOrder = i * batchSize + j + 1
-                    if (j > 0) {
-                        peers.add(prepareLeaderboardUser(uid, accountsBatch[j - 1]))
-                    } else if (i > 0) {
-                        val previousPage =
-                            accountRepository.findAllByOrderByPointsDesc(PageRequest.of((i - 1).toInt(), batchSize))
-                        peers.add(prepareLeaderboardUser(uid, previousPage[batchSize - 1]))
-                    }
-                    peers.add(prepareLeaderboardUser(uid, accountsBatch[j]))
-                    if (accountsBatch.size > 2 && j < batchSize - 1) {
-                        peers.add(prepareLeaderboardUser(uid, accountsBatch[j + 1]))
-                    } else if (accountsBatch.size == batchSize) {
-                        val nextPage =
-                            accountRepository.findAllByOrderByPointsDesc(PageRequest.of((i + 1).toInt(), batchSize))
-                        peers.add(prepareLeaderboardUser(uid, nextPage[0]))
-                    }
-                    break@iter
-                }
-            }
-        }
+    fun getLeaderboard(uuid: String): LeaderboardDto {
+        val top3 = findTop3Accounts(uuid)
+        val currentAcc = accountRepository.findByUid(uuid)
+        val (myOrder, peers) = currentAcc?.points?.let { points ->
+            positionInLeaderBoard(points) to findAccountPeers(points, uuid)
+        } ?: (0 to emptyList())
 
         return LeaderboardDto(
             top = top3,
             peers = peers,
-            myOrder = userOrder.toInt()
+            myOrder = myOrder
         )
     }
+
+    private fun findAccountPeers(
+        points: Int,
+        uuid: String
+    ): List<LeaderboardUserDto> = accountRepository.findPeers(points).map { prepareLeaderboardUser(uuid, it) }
+
+    private fun positionInLeaderBoard(points: Int) = accountRepository.findNumberOfAccountsAbove(points) + 1
 
     private fun findTop3Accounts(uid: String) = accountRepository.findAllByOrderByPointsDesc(PageRequest.of(0, 3)).map {
         prepareLeaderboardUser(uid, it)
--- backend/src/main/kotlin/cz/loono/backend/db/repository/AccountRepository.kt
+++ backend/src/main/kotlin/cz/loono/backend/db/repository/AccountRepository.kt
@@ -1,8 +1,10 @@
 package cz.loono.backend.db.repository
 
 import cz.loono.backend.db.model.Account
+import org.intellij.lang.annotations.Language
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -10,4 +12,23 @@ interface AccountRepository : JpaRepository<Account, Long> {
     fun existsByUid(uid: String): Boolean
     fun findByUid(uid: String): Account?
     fun findAllByOrderByPointsDesc(pageable: Pageable): List<Account>
+    // Looks for first account which has more and fewer points than current one
+    @Language("SQL")
+    @Query(
+        """
+            (SELECT * FROM account WHERE points <= :points ORDER BY points DESC limit 2) 
+            UNION DISTINCT 
+            (SELECT * FROM account WHERE points >= :points ORDER BY points ASC limit 2) ORDER BY points DESC
+        """,
+        nativeQuery = true
+    )
+    fun findPeers(points: Int): List<Account>
+    @Language("SQL")
+    @Query(
+        """
+            SELECT COUNT(*) FROM account WHERE points > :points
+        """,
+        nativeQuery = true
+    )
+    fun findNumberOfAccountsAbove(points: Int): Int
 }
