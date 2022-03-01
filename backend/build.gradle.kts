e0159068502964cbbac17e7a37410f09c13db959
Dan Zaru <d35h@users.noreply.github.com>
2022-03-01 21:18:04 +0100

    fix-logging-level: Fix logging (#67)
    * fix-logging-level: Fix logging

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -34,9 +34,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
 
     implementation("com.google.firebase:firebase-admin:8.1.0")
-    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.5")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
 
-    implementation("org.slf4j:slf4j-api:2.0.0-alpha6")
+    implementation("org.slf4j:slf4j-api:1.7.36")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("ch.qos.logback:logback-core:$logbackVersion")
 
