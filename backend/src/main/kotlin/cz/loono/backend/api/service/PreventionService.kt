0297f105f47fe3e6120af0ef28cc88c529032a53
jiri.dobes <jiri.dobes@cgi.com>
2022-07-22 15:28:41 +0200

    #50 remove default sex validation, add type specific validation

--- backend/src/main/kotlin/cz/loono/backend/api/service/PreventionService.kt
+++ backend/src/main/kotlin/cz/loono/backend/api/service/PreventionService.kt
@@ -154,6 +154,6 @@ class PreventionService(
         when (type) {
             SelfExaminationTypeDto.BREAST -> sex == SexDto.FEMALE.name
             SelfExaminationTypeDto.TESTICULAR -> sex == SexDto.MALE.name
-            else -> true
+            SelfExaminationTypeDto.SKIN -> true
         }
 }
