d1c9854eb8722a78fdcf50313418a03396bc336b
stepan.drozdek <s.drozdek@centrum.cz>
2022-09-26 15:36:39 +0200

    klint fix

--- backend/src/test/kotlin/cz/loono/backend/api/service/PreventionServiceTest.kt
+++ backend/src/test/kotlin/cz/loono/backend/api/service/PreventionServiceTest.kt
@@ -1,6 +1,7 @@
 package cz.loono.backend.api.service
 
 import cz.loono.backend.api.dto.BadgeTypeDto
+import cz.loono.backend.api.dto.ExaminationCategoryTypeDto
 import cz.loono.backend.api.dto.ExaminationPreventionStatusDto
 import cz.loono.backend.api.dto.ExaminationStatusDto
 import cz.loono.backend.api.dto.ExaminationTypeDto
@@ -8,7 +9,6 @@ import cz.loono.backend.api.dto.SelfExaminationPreventionStatusDto
 import cz.loono.backend.api.dto.SelfExaminationResultDto
 import cz.loono.backend.api.dto.SelfExaminationStatusDto
 import cz.loono.backend.api.dto.SelfExaminationTypeDto
-import cz.loono.backend.api.dto.ExaminationCategoryTypeDto
 import cz.loono.backend.createAccount
 import cz.loono.backend.db.model.ExaminationRecord
 import cz.loono.backend.db.model.SelfExaminationRecord
