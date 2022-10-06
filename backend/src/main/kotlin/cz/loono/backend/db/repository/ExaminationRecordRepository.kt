dcb851d6dc2ea6c96fd6ca33867c2880618698ec
stepan.drozdek <s.drozdek@centrum.cz>
2022-10-06 09:22:50 +0200

    klint fix

--- backend/src/main/kotlin/cz/loono/backend/db/repository/ExaminationRecordRepository.kt
+++ backend/src/main/kotlin/cz/loono/backend/db/repository/ExaminationRecordRepository.kt
@@ -3,9 +3,7 @@ package cz.loono.backend.db.repository
 import cz.loono.backend.api.dto.ExaminationStatusDto
 import cz.loono.backend.db.model.Account
 import cz.loono.backend.db.model.ExaminationRecord
-import org.intellij.lang.annotations.Language
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
