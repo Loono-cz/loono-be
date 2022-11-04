1f62f550b7951793c656a6ba8dc5f19798f5016a
stepan.drozdek <s.drozdek@centrum.cz>
2022-11-04 14:41:25 +0100

    changes from devel to prod

--- infrastructure/aws/backend.tf
+++ infrastructure/aws/backend.tf
@@ -129,8 +129,8 @@ resource "aws_ecs_task_definition" "backend" {
   network_mode          = "awsvpc"
   execution_role_arn    = aws_iam_role.ecs-task-execution-role.arn
   task_role_arn         = aws_iam_role.ecs-task-execution-role.arn
-  memory                = "1024"
-  cpu                   = "256"
+  memory                = "2048"
+  cpu                   = "512"
 }
 
 resource "aws_ecs_service" "backend" {
@@ -141,7 +141,7 @@ resource "aws_ecs_service" "backend" {
   desired_count                      = 2
   deployment_minimum_healthy_percent = 100
   deployment_maximum_percent         = 200
-  health_check_grace_period_seconds  = 20
+  health_check_grace_period_seconds  = 250
 
   network_configuration {
     security_groups = [
@@ -219,8 +219,9 @@ resource "aws_lb_target_group" "backend-tg" {
   target_type = "ip"
   health_check {
     path                = "/actuator/health"
-    unhealthy_threshold = 3
-    interval            = 10
+    unhealthy_threshold = 10
+    interval            = 30
+    timeout             = 9
     enabled             = true
     port                = 8080
     matcher             = "200"
