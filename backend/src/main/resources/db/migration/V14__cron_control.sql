5981e8158a873d3eed25f0435f095a25c7bb0d0b
stepan.drozdek <s.drozdek@centrum.cz>
2022-12-19 15:07:30 +0100

    Create V14__cron_control.sql

---
+++ backend/src/main/resources/db/migration/V14__cron_control.sql
@@ -0,0 +1,14 @@
+ALTER TABLE notification_log ADD created_at text;
+
+CREATE SEQUENCE cron_control_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE user_feedback (
+                                  id bigint PRIMARY KEY DEFAULT nextval('cron_control_seq'),
+                                  message text,
+                                  created_at text
+);
