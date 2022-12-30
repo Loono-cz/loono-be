6c75ebbcbc458238da196e61bcc40fdfac1b52ae
stepan.drozdek <s.drozdek@centrum.cz>
2022-12-30 15:13:34 +0100

    save cron calls in cronLog

--- backend/src/main/kotlin/cz/loono/backend/schedule/LoonoTaskScheduler.kt
+++ backend/src/main/kotlin/cz/loono/backend/schedule/LoonoTaskScheduler.kt
@@ -1,23 +1,29 @@
 package cz.loono.backend.schedule
 
+import cz.loono.backend.db.model.CronLog
+import cz.loono.backend.db.repository.CronLogRepository
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.time.LocalDate
 
 @Component
 class LoonoTaskScheduler(
-    private val dailyTasks: List<DailySchedulerTask>
+    private val dailyTasks: List<DailySchedulerTask>,
+    private val cronLogRepository: CronLogRepository
 ) {
     @Scheduled(cron = "\${scheduler.cron.daily-task}") // each day at 3AM
     fun executeDailyTasks() {
         // dailyTasks.forEach(DailySchedulerTask::run)
-        BadgeDowngradeTask::run
-        ComingAndPassedExamNotificationTask::run
-        CustomExamStatusChangeTask::run
-        ExaminationCancellationTask::run
-        PlanNewExamReminderTask::run
-        PreventionReminderTask::run
-        SelfExaminationIntervalClosingTask::run
-        SelfExamReminderTask::run
-        SelfExaminationWaitingTask::run
+        dailyTasks.forEach { task ->
+            task.run()
+            cronLogRepository.save(
+                CronLog(
+                    functionName = "${task.javaClass::getName}",
+                    status = "RUN CHECK",
+                    message = "$task",
+                    createdAt = LocalDate.now().toString()
+                )
+            )
+        }
     }
 }
