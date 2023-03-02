d4fd5dee2307ddbec5b3b8982e90d25763eb0d9b
stepan.drozdek <s.drozdek@centrum.cz>
2023-03-02 14:36:21 +0100

    klint

--- backend/src/main/kotlin/cz/loono/backend/api/service/HealthcareProvidersService.kt
+++ backend/src/main/kotlin/cz/loono/backend/api/service/HealthcareProvidersService.kt
@@ -29,7 +29,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
