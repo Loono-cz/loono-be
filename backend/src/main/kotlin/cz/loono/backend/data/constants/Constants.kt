eb5ee62decc98f43340989bbeaf7332c18799552
stepan.drozdek <s.drozdek@centrum.cz>
2023-03-23 14:58:34 +0100

    providers from google docs

--- backend/src/main/kotlin/cz/loono/backend/data/constants/Constants.kt
+++ backend/src/main/kotlin/cz/loono/backend/data/constants/Constants.kt
@@ -5,6 +5,9 @@ object Constants {
     const val OPEN_DATA_URL =
         "https://opendata.mzcr.cz/data/nrpzs/narodni-registr-poskytovatelu-zdravotnich-sluzeb.csv"
 
+    const val CORRECTED_DATA_URL =
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vQOpp7Y7eMlDgP-avxqxLHzFvigIoiqXVIYWCTe4UIT5IemWKr2lu6FCiwVqpNhSrLAXm5FV0XOMRkt/pub?gid=486149684&single=true&output=xlsx"
+
     val healthcareProvidersCSVHeader = listOf(
         // IDs - location ID, institution ID
         "MistoPoskytovaniId", "ZdravotnickeZarizeniId",
